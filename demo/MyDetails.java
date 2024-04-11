5f8f0c2ee58913ea07899961e5e571e13ede51de
LokkyManoj <113964739+LokkyManoj@users.noreply.github.com>
2024-04-11 17:43:26 +0530

    Add files via upload

---
+++ demo/MyDetails.java
@@ -0,0 +1,14 @@
+package com.chainsys.demo;
+
+public class MyDetails {
+
+	public static void main(String[] args) {
+		System.out.println("Name: Manoj.P");
+		System.out.println("Designation: Java Trainee");
+		System.out.println("Location: Madurai");
+		System.out.println("College Name: Sethu Institute of Technology");
+		
+
+	}
+
+}
